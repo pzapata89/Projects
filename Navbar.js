a0d9c7b27746ce76265eb673d2a95f39b2a6fe05
Pedro Zapata <pzapata89@users.noreply.github.com>
2017-05-16 11:38:23 -0400

    Archivos raiz
    Archivos html y php de todo el proyecto

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+
+if ($(window).width() < 700) {
+  $("#next").hide();
+};
+
+/*$(window).on("resize", function() {
+  if ($(window).width() < 700) {
+    $("#next").hide();
+  }
+  else {
+    $("#next").show();
+  }
+});*/
